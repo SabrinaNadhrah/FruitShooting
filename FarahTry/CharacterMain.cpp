b142b730f5cb070e8ff03e39b6060ef2c2a65384
NUR FARAH ADIBAH <diebah.work111@gmail.com>
2023-06-15 22:10:46 +0800

    update

--- FarahTry/CharacterMain.cpp
+++ FarahTry/CharacterMain.cpp
@@ -7,41 +7,38 @@
 #include "bullet.h"
 #include "bullet.cpp"
 
-
 using namespace std;
 
 int main()
 {
     int screenWidth = getmaxwidth();
     int screenHeight = getmaxheight();
-    
+
     initwindow(screenWidth, screenHeight, "Fruit Shooting Character part");
     setbkcolor(WHITE); // Set the background color to white
 
-    void* background;
+    void *background;
     int imageWidth = screenWidth;
     int imageHeight = screenHeight;
     int size = imagesize(0, 0, imageWidth, imageHeight);
     background = new char[size];
 
-    
-
-    int character2X = screenWidth / 2 + 100;// start at the middle of screen
-    int character2Y = screenHeight - 230; 
+    int character2X = screenWidth / 2 + 100; // start at the middle of screen
+    int character2Y = screenHeight - 230;
 
     Character character2("Player", character2X, character2Y);
 
-    Weapon w1(screenWidth/2, screenHeight-100);
-	char ch;
+    Weapon w1(screenWidth / 2, screenHeight - 100);
+    char ch;
 
+    int direction = 1;
 
-    int direction = 1 ;
-    while (!kbhit())
+    while (true)
     {
+        delay(40);
         setactivepage(1);
         cleardevice();
 
-        
         // Update character 2 position
         character2.moveRight();
         if (character2.getPosition() >= screenWidth)
@@ -52,32 +49,30 @@ int main()
 
         setvisualpage(1);
 
-        
         // Move the weapon
         w1.move(5 * direction);
 
         // Reverse direction if the weapon reaches the screen boundaries
-        if (w1.getPosition() <= 0 || w1.getPosition() >= (screenWidth - 155) )
+        if (w1.getPosition() <= 0 || w1.getPosition() >= (screenWidth - 155))
             direction *= -1;
 
-		w1.doAction();
+        w1.doAction();
 
-		if (kbhit())
-		{
-			ch = getch();
+        if (kbhit())
+        {
 
-			if (ch == 27)
-				break;
-			else if (ch == ' ')
-				w1.shoot();
-		}
+            ch = getch();
+
+            if (ch == 27)
+                break;
+            else if (ch == ' ')
+                w1.shoot();
+        }
 
         // Redraw the weapon
         cleardevice();
         w1.update();
         w1.draw();
-
-        delay(40);
     }
 
     closegraph();
